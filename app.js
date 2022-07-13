c6d4002772100e4dd88dda7a3363b87e93afbc62
Feanil Patel <feanil@tcril.org>
2022-07-13 09:16:46 -0500

    fix: Don't set a default redirect url.
    If a redirect url is not specified, then don't start up.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const redirect_url = process.env.REDIRECT_URL || "https://example.com"
+const redirect_url = process.env.REDIRECT_URL
+
+if (typeof redirect_url === 'undefined') {
+  throw new Error("REDIRECT_URL environment variable is undefined.")
+}
 
 app.get('/', (req, res) => {
     res.redirect(redirect_url)
